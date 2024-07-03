341f72e3e71d3a3f57fe751cbee11bc4d9541620
Seyam <seyam.hossain.118833@gmail.com>
2024-07-03 18:19:29 +0600

    desktop nav done. forwarding project v-1.0

--- client/src/Components/subComponents/UserProfile/UserProfile.jsx
+++ client/src/Components/subComponents/UserProfile/UserProfile.jsx
@@ -1,7 +1,18 @@
+import image from "../../../assets/user.png";
+import { ArrowDataTransferVerticalIcon } from "../../../icons/icons";
+import styles from "./UserProfile.module.css";
+
 export default function UserProfile() {
   return (
-    <>
-      <div>user profile</div>
-    </>
+    <div className={styles.user_profile}>
+      <div className={styles.user_icon}>
+        <img src={image} alt="user" />
+      </div>
+      <div>
+        <h4 className={`${styles.user_name} text-primary`}>Username</h4>
+        <p className={`${styles.user_desc} text-secondary`}>Description</p>
+      </div>
+      <ArrowDataTransferVerticalIcon className="text-primary" />
+    </div>
   );
 }
