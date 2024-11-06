6acadecc38ffff404de8926489625efce3dba8ad
Seyam <seyam.hossain.118833@gmail.com>
2024-11-07 03:03:06 +0600

    login page added

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,14 +1,16 @@
 import LoginForm from "../../Components/LoginForm/LoginForm";
 import ToogleMode from "../../Components/ToogleMode/ToogleMode";
+import styles from "./Login.module.css";
 
 export default function Login() {
   return (
     <div>
-      <div className="flex md:flex-row flex-col">
-        <div className="bg-secondary basis-full md:basis-1/6 md:min-h-screen">
-          <ToogleMode />
-        </div>
-        <div className="bg-primary basis-full md:basis-5/6 min-h-screen p-2 md:p-8">
+      <div className="bg-primary flex flex-row items-center min-h-screen">
+        <div className="basis-full p-2 md:p-8 ">
+          <div className={styles.toogleMode_holder}>
+            <ToogleMode customClass={"drop-shadow-sm"} />
+          </div>
+
           <LoginForm />
         </div>
       </div>
