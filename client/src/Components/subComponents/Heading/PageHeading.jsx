0ed159a9f4759c505f49e572384d62770ce49766
Seyam <seyam.hossain.118833@gmail.com>
2024-11-14 00:21:04 +0600

    PageHeading component ficed

--- client/src/Components/subComponents/Heading/PageHeading.jsx
+++ client/src/Components/subComponents/Heading/PageHeading.jsx
@@ -1,8 +1,13 @@
 import PropTypes from "prop-types";
 
-export default function PageHeading({ children }) {
-  return <h1 className="text-primary text-2xl font-semibold">{children}</h1>;
+export default function PageHeading({ children, customClass }) {
+  return (
+    <h1 className={`text-primary text-2xl font-semibold ${customClass}`}>
+      {children}
+    </h1>
+  );
 }
 PageHeading.propTypes = {
   children: PropTypes.string.isRequired,
+  customClass: PropTypes.string,
 };
