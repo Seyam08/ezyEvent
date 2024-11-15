1f074bd9c76f892351a15f7a42c12f43f3ca8433
Seyam <seyam.hossain.118833@gmail.com>
2024-11-16 04:17:45 +0600

    TooltipIcon link addes

--- client/src/Components/subComponents/AnimatedIcons/TooltipIcon.jsx
+++ client/src/Components/subComponents/AnimatedIcons/TooltipIcon.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export default function TooltipIcon({ text, icon: Icon }) {
+export default function TooltipIcon({ text, icon: Icon, link }) {
   return (
     <div className="group relative w-6 h-6">
-      <button>
+      <Link to={link ? link : "#"} target={"_blank"}>
         <Icon className="w-full h-full hover:scale-125 duration-200 text-secondary hover:text-[#8C5BFE]" />
-      </button>
+      </Link>
       <span className="absolute -top-14 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 px-3 rounded-lg border-thin bg-primary text-secondary py-2 text-sm font-bold shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
         {text}
       </span>
@@ -15,4 +16,5 @@ export default function TooltipIcon({ text, icon: Icon }) {
 TooltipIcon.propTypes = {
   text: PropTypes.string.isRequired,
   icon: PropTypes.func.isRequired,
+  link: PropTypes.string,
 };
