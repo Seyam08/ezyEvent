efba6faaef9b3128470b21e38e8e474f40b0c661
Seyam <seyam.hossain.118833@gmail.com>
2024-11-17 23:11:24 +0600

    SpeakerList component builded

---
+++ client/src/Components/SpeakerList/SpeakerList.jsx
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export default function SpeakerList({ list, link, customClass }) {
+  return (
+    <div
+      className={`p-6 bg-secondary rounded-lg shadow-md max-w-md mx-auto ${customClass}`}
+    >
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-lg font-semibold text-primary">
+          Event Speaker List
+        </h2>
+        <Link
+          to={link}
+          className="text-secondary font-medium bg-tertiary rounded-full px-4 py-1 hover:foreground"
+        >
+          View All
+        </Link>
+      </div>
+      <ul className="space-y-4">
+        {list.map((speaker, id) => (
+          <li
+            key={id}
+            className="flex items-center space-x-4 border-b border-gray-300 dark:border-gray-700 pb-4 last:border-b-0 "
+          >
+            <div className="w-12 h-12 cursor-pointer">
+              <img
+                src={speaker.avatar}
+                alt={speaker.name}
+                className="p-1 rounded-full ring-1 ring-[#8C5BFE] h-full w-full"
+              />
+            </div>
+
+            <div className="flex-1">
+              <h3 className="text-sm font-semibold text-secondary">
+                {speaker.name}
+              </h3>
+              <p className="text-xs text-tertiary">{speaker.designation}</p>
+            </div>
+            <div className="text-right">
+              <p className="text-xs text-tertiary">Upcoming Event</p>
+              <p className="text-sm text-secondary font-medium">
+                {speaker.upcomingEvent}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+SpeakerList.propTypes = {
+  list: PropTypes.array.isRequired,
+  link: PropTypes.string.isRequired,
+  customClass: PropTypes.string,
+};
---
+++ client/src/constants/speakersList.js
@@ -0,0 +1,28 @@
+import image from "../assets/user.png";
+
+export const speakersList = [
+  {
+    name: "Charles Williams",
+    designation: "Lilyen Demi",
+    upcomingEvent: "Nov 7",
+    avatar: image,
+  },
+  {
+    name: "Leslie Alexander",
+    designation: "Creative Director",
+    upcomingEvent: "Nov 12",
+    avatar: image,
+  },
+  {
+    name: "Courtney Henry",
+    designation: "Lilyen Demi",
+    upcomingEvent: "Nov 15",
+    avatar: image,
+  },
+  {
+    name: "Darlene Robertson",
+    designation: "Digital Marketer",
+    upcomingEvent: "Nov 23",
+    avatar: image,
+  },
+];
