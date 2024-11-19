fd9aa8ac079d3f064760cf344004893c41e2ca36
Seyam <seyam.hossain.118833@gmail.com>
2024-11-20 02:59:21 +0600

    useCounter fixed

--- client/src/hooks/useCounter.js
+++ client/src/hooks/useCounter.js
@@ -1,34 +1,30 @@
 import { useEffect, useState } from "react";
 
-
 const useCounter = (limit) => {
+  if (typeof limit !== "number") {
+    throw new Error("The limit of useCounter hook must be a number");
+  }
 
-    if (typeof limit !== "number") {
-        throw new Error("The limit of useCounter hook must be a number");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count < limit) {
+      const interval = setInterval(() => {
+        setCount((prevCount) => {
+          if (prevCount < limit) {
+            return prevCount + 1;
+          } else {
+            clearInterval(interval);
+            return prevCount;
+          }
+        });
+      }, 1); // Adjust the interval time for the desired counting speed
+
+      return () => clearInterval(interval);
     }
+  }, [count, limit]);
 
-    const [count, setCount] = useState(0);
+  return count;
+};
 
-    useEffect(() => {
-
-        if (count < limit) {
-            
-        const interval = setInterval(() => {
-            setCount((prevCount) => {
-            if (prevCount < limit) {
-                return prevCount + 9;
-            } else {
-                clearInterval(interval);
-                return prevCount;
-            }
-            });
-        }, 10); // Adjust the interval time for the desired counting speed
-
-        return () => clearInterval(interval);
-        }
-    }, [count, limit]);
-
-    return count;
-    }
-
-export default useCounter
+export default useCounter;
