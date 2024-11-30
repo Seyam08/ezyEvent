8f99dd91ae45dd17d21dbfc29e4211859ee30263
Seyam <seyam.hossain.118833@gmail.com>
2024-12-01 05:11:49 +0600

    ErrorMsgBox component created

---
+++ client/src/Components/subComponents/ErrorMsgBox/ErrorMsgBox.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+
+export default function ErrorMsgBox({ children, bgColor, txtColor }) {
+  return (
+    <div
+      className={`${txtColor} ${bgColor} bg-opacity-10 px-4 py-2 rounded-md text-sm animate-fade-up animate-duration-300 animate-ease-in`}
+    >
+      {children}
+    </div>
+  );
+}
+
+ErrorMsgBox.propTypes = {
+  children: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  txtColor: PropTypes.string.isRequired,
+};
