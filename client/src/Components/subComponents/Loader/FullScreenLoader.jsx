2e37ab8b87a8662c2c98d8c4e418d0be01db5456
Seyam <seyam.hossain.118833@gmail.com>
2024-12-01 19:43:19 +0600

    FullScreenLoader component builded

---
+++ client/src/Components/subComponents/Loader/FullScreenLoader.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import styles from "./FullScreenLoader.module.css";
+
+export default function FullScreenLoader({ color }) {
+  return (
+    <div className={`${styles.loaderOverlay} bg-tertiary bg-opacity-5`}>
+      <div className={`${styles.loaderContainer} bg-secondary box-shadow`}>
+        <div className="grid grid-cols-3 gap-1 w-[70px] h-[70px]">
+          <span
+            className={`${styles.loaderSpan} ${color}`}
+            style={{ animationDelay: "0ms" }}
+          ></span>
+          <span
+            className={`${styles.loaderSpan} ${color}`}
+            style={{ animationDelay: "200ms" }}
+          ></span>
+          <span
+            className={`${styles.loaderSpan} ${color}`}
+            style={{ animationDelay: "300ms" }}
+          ></span>
+          <span
+            className={`${styles.loaderSpan} ${color}`}
+            style={{ animationDelay: "400ms" }}
+          ></span>
+          <span
+            className={`${styles.loaderSpan} ${color}`}
+            style={{ animationDelay: "500ms" }}
+          ></span>
+          <span
+            className={`${styles.loaderSpan} ${color}`}
+            style={{ animationDelay: "600ms" }}
+          ></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+FullScreenLoader.propTypes = {
+  color: PropTypes.string.isRequired,
+};
