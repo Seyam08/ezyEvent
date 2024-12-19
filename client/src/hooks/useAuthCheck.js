1d62a263b8755464da1dd82e26e732c5dbb0b058
Seyam <seyam.hossain.118833@gmail.com>
2024-12-19 17:57:48 +0600

    hook for sync login

---
+++ client/src/hooks/useAuthCheck.js
@@ -0,0 +1,40 @@
+import { jwtVerify } from "jose";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useLogoutMutation } from "../features/auth/authApi";
+import { userLoggedIn } from "../features/auth/authSlice";
+
+export default function useAuthCheck() {
+  const dispatch = useDispatch();
+  const [authChecked, setAuthChecked] = useState(false);
+  const [logout] = useLogoutMutation();
+
+  useEffect(() => {
+    const localAuth = localStorage?.getItem("auth");
+
+    if (localAuth) {
+      // Convert the secret key to Uint8Array
+      const secretKey = new TextEncoder().encode(
+        import.meta.env.VITE_JWT_SECRET
+      );
+      // Verify the token
+      jwtVerify(localAuth, secretKey)
+        .then(({ payload }) => {
+          if (payload) {
+            dispatch(
+              userLoggedIn({
+                profile: payload,
+              })
+            );
+          }
+        })
+        .catch(() => {
+          logout();
+        });
+    }
+
+    setAuthChecked(true);
+  }, [dispatch, logout, setAuthChecked]);
+
+  return authChecked;
+}
