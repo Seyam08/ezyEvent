67e15971355a201e70a96c81d49b99eebfabbfa2
Seyam <seyam.hossain.118833@gmail.com>
2025-01-12 11:06:20 +0600

    client - fix: add fallback image for ProfileCard component on image load error

--- client/src/Components/ProfileCard/ProfileCard.jsx
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import defaultImage from "../../assets/avatar.svg";
 import ItemHeading from "../../Components/subComponents/Heading/ItemHeading";
 import { FacebookIcon, LinkedinIcon, NewTwitterIcon } from "../../icons/icons";
 import TooltipIcon from "../subComponents/AnimatedIcons/TooltipIcon";
@@ -7,11 +9,15 @@ export default function ProfileCard({
   name,
   designation,
   avatar,
+  link = "#",
   facebook,
   linkedIn,
   X,
   customClass,
 }) {
+  const handleImageError = (event) => {
+    event.target.src = defaultImage;
+  };
   return (
     <div
       className={`rounded-lg px-4 py-6 flex flex-col gap-3 items-center text-center ${customClass}`}
@@ -19,11 +25,16 @@ export default function ProfileCard({
       <div className="w-20 h-20 cursor-pointer">
         <img
           src={avatar}
+          onError={handleImageError}
           alt="Avatar"
           className="p-1 rounded-full ring-1 ring-[#514cfe] h-full w-full"
         />
       </div>
-      <ItemHeading>{name}</ItemHeading>
+      <ItemHeading>
+        <Link to={link} className="text-glow">
+          {name}
+        </Link>
+      </ItemHeading>
       <p className="text-sm text-tertiary">{designation}</p>
       <div className="bg-tertiary flex flex-row w-full justify-center space-x-5 py-2 rounded-md">
         {facebook ? (
@@ -41,8 +52,9 @@ ProfileCard.propTypes = {
   name: PropTypes.string.isRequired,
   designation: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
+  link: PropTypes.string,
   facebook: PropTypes.string,
-  linkdin: PropTypes.string,
+  linkedIn: PropTypes.string,
   X: PropTypes.string,
   customClass: PropTypes.string,
 };
