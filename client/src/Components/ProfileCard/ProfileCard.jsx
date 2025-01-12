778856157e8e1a60a18aa2150a6923c2a46c3568
Seyam <seyam.hossain.118833@gmail.com>
2025-01-12 14:16:54 +0600

    client - fixed icon size

--- client/src/Components/ProfileCard/ProfileCard.jsx
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -18,9 +18,6 @@ export default function ProfileCard({
   const handleImageError = (event) => {
     event.target.src = defaultImage;
   };
-  const handleImageError = (event) => {
-    event.target.src = defaultImage;
-  };
   return (
     <div
       className={`rounded-lg px-4 py-6 flex flex-col gap-3 items-center text-center ${customClass}`}
@@ -28,7 +25,6 @@ export default function ProfileCard({
       <div className="w-20 h-20 cursor-pointer">
         <img
           src={avatar}
-          onError={handleImageError}
           onError={handleImageError}
           alt="Avatar"
           className="p-1 rounded-full ring-1 ring-[#514cfe] h-full w-full"
@@ -42,12 +38,32 @@ export default function ProfileCard({
       <p className="text-sm text-tertiary">{designation}</p>
       <div className="bg-tertiary flex flex-row w-full justify-center space-x-5 py-2 rounded-md">
         {facebook ? (
-          <TooltipIcon text="Facebook" icon={FacebookIcon} link={facebook} />
+          <TooltipIcon
+            h="h-5"
+            w="w-5"
+            text="Facebook"
+            icon={FacebookIcon}
+            link={facebook}
+          />
         ) : null}
         {linkedIn ? (
-          <TooltipIcon text="LinkedIn" icon={LinkedinIcon} link={linkedIn} />
+          <TooltipIcon
+            h="h-5"
+            w="w-5"
+            text="LinkedIn"
+            icon={LinkedinIcon}
+            link={linkedIn}
+          />
         ) : null}
-        {X ? <TooltipIcon text="X" icon={NewTwitterIcon} link={X} /> : null}
+        {X ? (
+          <TooltipIcon
+            h="h-5"
+            w="w-5"
+            text="X"
+            icon={NewTwitterIcon}
+            link={X}
+          />
+        ) : null}
       </div>
     </div>
   );
